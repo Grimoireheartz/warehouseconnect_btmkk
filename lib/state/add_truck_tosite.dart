274e51410e8365612d360d8734019ec1ee6feebd
techin <techin.naja@gmail.com>
2023-03-17 17:10:32 +0700

    done matching serial data from data base

--- lib/state/add_truck_tosite.dart
+++ lib/state/add_truck_tosite.dart
@@ -23,6 +23,20 @@ class _AddTruckToSiteState extends State<AddTruckToSite> {
   List<TextEditingController> truckdata_serial = [
     for (int i = 1; i <= 11; i++) TextEditingController()
   ];
+  List<Icon> truckicon_display = [
+    for (int i = 1; i <= 11; i++)
+      Icon(
+        Icons.info_outline,
+        color: Colors.black,
+      )
+  ];
+  List<BorderSide> truckbordercolors_display = [
+    for (int i = 1; i <= 11; i++) BorderSide(color: Colors.grey)
+  ];
+  List<String> truckGetData_model = [for (int i = 1; i <= 11; i++) ''];
+
+  List<TruckInfoModel> truckData_toAdd = [];
+
   final formKey = GlobalKey<FormState>();
 
   @override
@@ -180,8 +194,39 @@ class _AddTruckToSiteState extends State<AddTruckToSite> {
                                   await Dio().get(apiPath).then((value) {
                                     if (value.toString() == 'null') {
                                       print('is Mull');
+                                      for (var x = 1;
+                                          x <= truckserialCount;
+                                          x++) {
+                                        setState(() {
+                                          truckicon_display[x] = Icon(
+                                            Icons.close,
+                                            color: Colors.red,
+                                          );
+
+                                          truckbordercolors_display[x] =
+                                              BorderSide(
+                                                  color: Color.fromARGB(
+                                                      255, 207, 11, 11));
+                                        });
+                                      }
                                     } else {
                                       // print(value);
+                                      for (var x = 1;
+                                          x <= truckserialCount;
+                                          x++) {
+                                        setState(() {
+                                          truckicon_display[x] = Icon(
+                                            Icons.close,
+                                            color: Colors.red,
+                                          );
+
+                                          truckbordercolors_display[x] =
+                                              BorderSide(
+                                                  color: Color.fromARGB(
+                                                      255, 207, 11, 11));
+                                        });
+                                      }
+
                                       for (var dataTruck
                                           in jsonDecode(value.data)) {
                                         TruckInfoModel truckInfoModel =
@@ -189,6 +234,42 @@ class _AddTruckToSiteState extends State<AddTruckToSite> {
                                         countdata++;
                                         print(
                                             'Data truck Model =>${truckInfoModel.serial_no} => ${truckInfoModel.item_no}');
+
+                                        for (var x = 1;
+                                            x <= truckserialCount;
+                                            x++) {
+                                          if (truckdata_serial[x].text ==
+                                              truckInfoModel.serial_no) {
+                                            setState(() {
+                                              truckGetData_model[x] =
+                                                  truckInfoModel.item_no;
+
+                                              var detectSerial = truckData_toAdd
+                                                  .any((element) =>
+                                                      element.serial_no ==
+                                                      truckInfoModel.serial_no);
+
+                                              if (detectSerial) {
+                                                print('Detect serial');
+                                              } else {
+                                                print('No detect serial');
+                                                truckData_toAdd
+                                                    .add(truckInfoModel);
+                                              }
+
+                                              truckicon_display[x] = Icon(
+                                                Icons.check,
+                                                color: Color.fromARGB(
+                                                    255, 4, 196, 33),
+                                              );
+
+                                              truckbordercolors_display[x] =
+                                                  BorderSide(
+                                                      color: Color.fromARGB(
+                                                          255, 4, 202, 21));
+                                            });
+                                          }
+                                        }
                                       }
                                     }
                                     setState(() {
@@ -212,6 +293,55 @@ class _AddTruckToSiteState extends State<AddTruckToSite> {
                 ),
               ),
             ),
+      bottomNavigationBar: BottomAppBar(
+        color: Color.fromARGB(255, 228, 225, 225),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: screensize * 0.8,
+              constraints: BoxConstraints(maxWidth: 800),
+              child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    alignment: Alignment.center,
+                    backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {
+                    for (var truck in truckData_toAdd) {
+                      print(truck.serial_no);
+                    }
+                  },
+                  icon: Icon(
+                    Icons.save_alt,
+                    size: 30,
+                    color: Colors.black,
+                  ),
+                  label: Container(
+                    height: 40,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Save truck to site',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        Text(
+                          'บันทึกข้อมูลไปยังไซต์',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.black,
+                          ),
+                        )
+                      ],
+                    ),
+                  )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -220,44 +350,53 @@ class _AddTruckToSiteState extends State<AddTruckToSite> {
 
     for (int x = 1; x <= truckserialCount; x++) {
       truckserialList.add(
-        Container(
-          margin: EdgeInsets.symmetric(vertical: 5),
-          width: screensize * 0.8,
-          constraints: BoxConstraints(maxWidth: 500),
-          child: TextFormField(
-            controller:
-                truckdata_serial[x] == null ? null : truckdata_serial[x],
-            validator: (value) {
-              if (value!.isEmpty) {
-                return '** โปรดกรอก Serial';
-              } else {
-                return null;
-              }
-            },
-            decoration: InputDecoration(
-              contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
-              errorStyle: TextStyle(color: Colors.red),
-              hintText: 'Serial Number',
-              filled: true,
-              fillColor: Colors.white,
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide:
-                    BorderSide(color: Color.fromARGB(255, 104, 103, 103)),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              errorBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(color: Colors.red, width: 2),
-              ),
-              prefixIcon: Icon(
-                Icons.info_outline,
-                color: Colors.black,
+        Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              width: screensize * 0.8,
+              constraints: BoxConstraints(maxWidth: 500),
+              child: TextFormField(
+                controller:
+                    truckdata_serial[x] == null ? null : truckdata_serial[x],
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return '** โปรดกรอก Serial';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
+                    errorStyle: TextStyle(color: Colors.red),
+                    hintText: 'Serial Number',
+                    filled: true,
+                    fillColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: truckbordercolors_display[x]),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.red, width: 2),
+                    ),
+                    prefixIcon: truckicon_display[x]),
               ),
             ),
-          ),
+            truckGetData_model[x].length > 0
+                ? Container(
+                    width: screensize * 0.8,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text('Model: ${truckGetData_model[x]}'),
+                      ],
+                    ),
+                  )
+                : Container()
+          ],
         ),
       );
     }
