250f508056a5a5f13105b467eb316b250f84d564
Grimoireheartz <120167489+Grimoireheartz@users.noreply.github.com>
2023-05-15 08:20:14 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\moblie_app\\test_fluttermoblie" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.5+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 5 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=D:\\moblie_app\\test_fluttermoblie"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\moblie_app\\test_fluttermoblie\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\moblie_app\\test_fluttermoblie"
+  "FLUTTER_TARGET=D:\\moblie_app\\test_fluttermoblie\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\moblie_app\\test_fluttermoblie\\.dart_tool\\package_config.json"
+)
